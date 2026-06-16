53e9ee1e3e846abcdf9cae72a7cbc7266ab7176f
raulabogados9-art <raulabogados9@gmail.com>
2026-06-16 17:59:23 -0600

    Change API_URL to window.API_URL

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbzq8PVkl-O4FDgaH1MBYBnKJ18DvNrHK5whBllm_cxc_KFL5SA9HPWcEo338FkCaeQndQ/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbzq8PVkl-O4FDgaH1MBYBnKJ18DvNrHK5whBllm_cxc_KFL5SA9HPWcEo338FkCaeQndQ/exec';
