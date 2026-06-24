cb46eaab4a58061f4c37360286e2c96402dba353
raulabogados9-art <raulabogados9@gmail.com>
2026-06-24 16:45:01 -0600

    Remove API_URL constant from auth.js
    Removed API_URL constant from auth.js.

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,3 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbxasYCrzKGGPTUMIJYYPifWqfswdRjdha65DQfegxGUfQAxHGQ-Ukv5Cn5CwzxJ9SQP6Q/exec';
-
 /* ==========================
 LOGIN
 ========================== */
