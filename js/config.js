3ee1d50694b360f044427cc7c8abcf9d3c36fa83
raulabogados9-art <raulabogados9@gmail.com>
2026-07-02 12:24:09 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbxKr27Qpur1HaYbZKGm6dZk2-k2laOsDy3NzKP8F4cfb5hyOezN7gBtLZEURGpsWwtICw/exec';
+const API_URL = "https://script.google.com/macros/s/AKfycbxKr27Qpur1HaYbZKGm6dZk2-k2laOsDy3NzKP8F4cfb5hyOezN7gBtLZEURGpsWwtICw/exec";
