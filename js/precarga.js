f655c0bd6a13db2ab332bacdc20c49c5cb504e04
raulabogados9-art <raulabogados9@gmail.com>
2026-07-09 17:52:13 -0600

    Update precarga.js

--- js/precarga.js
+++ js/precarga.js
@@ -1,1 +1,42 @@
+/* ==========================================
+PRECARGA INTELIGENTE
+========================================== */
 
+window.precargarSistema = async function(){
+
+try{
+
+const tareas=[];
+
+if(cacheSistema.personas.length===0){
+
+tareas.push(cargarPersonas());
+
+}
+
+if(cacheSistema.actividades.length===0){
+
+tareas.push(cargarActividades());
+
+}
+
+if(cacheSistema.usuarios.length===0){
+
+tareas.push(cargarUsuarios());
+
+}
+
+await Promise.all(tareas);
+
+console.log("Precarga completada");
+
+}catch(e){
+
+console.error(
+"Error precarga:",
+e
+);
+
+}
+
+}
