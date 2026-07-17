b3e0de96b14551fccbc4f016572af1c49fd490ad
raulabogados9-art <raulabogados9@gmail.com>
2026-07-17 10:06:40 -0600

    Create exportar_historico.js

---
+++ js/exportar_historico.js
@@ -0,0 +1,24 @@
+// ==========================================
+// EXPORTAR HISTÓRICO
+// M&S ABOGADOS CONSULTORES
+// ==========================================
+
+// Cargar logo
+async function cargarLogo(){
+
+}
+
+// Crear encabezado del reporte
+function crearEncabezadoReporte(){
+
+}
+
+// Ajustar columnas
+function ajustarColumnas(){
+
+}
+
+// Exportar Histórico
+async function exportarHistorico(){
+
+}
