0d71d99753cd7749ee0a9ec37e9a8df08fd6b312
raulabogados9-art <raulabogados9@gmail.com>
2026-07-27 17:23:25 -0600

    Create modulos.js

---
+++ js/modulos.js
@@ -0,0 +1,23 @@
+CARGADORES_MODULOS = {
+
+expedientes:{
+funcion:"mostrarModulo"
+},
+
+personas:{
+funcion:null
+},
+
+usuarios:{
+funcion:null
+},
+
+reportes:{
+funcion:null
+},
+
+bitacora:{
+funcion:null
+}
+
+}
